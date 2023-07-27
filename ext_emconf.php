b0c0a2d8ebf31cbb51e7ac56d5f0ce4b39ffc2ef
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-07-27 11:39:29 +0200

    [TASK] initial 12 compat definition to start tests

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.2.99',
-            'typo3' => '11.5.0-11.5.99'
+            'php' => '8.1.0-8.2.99',
+            'typo3' => '12.4.0-12.4.99'
         ]
     ],
     'autoload' => [
